bdf99af32b748bdc51ecfa04f6ba6d03dd649f3e
icealtria <74185220+icealtria@users.noreply.github.com>
2024-05-18 15:26:20 +0800

    add url tip to GUI

--- linkwiz/gui.py
+++ linkwiz/gui.py
@@ -5,8 +5,45 @@ from typing import Dict
 import logging
 
 
-class LinkwizGUI:
+class ToolTip:
+    """Class to manage tooltips for Tkinter widgets."""
 
+    def __init__(self, widget, text):
+        self.widget = widget
+        self.text = text
+        self.tip_window = None
+        self.widget.bind("<Enter>", self.show_tooltip)
+        self.widget.bind("<Leave>", self.hide_tooltip)
+
+    def show_tooltip(self, event=None):
+        """Display the tooltip."""
+        if self.tip_window or not self.text:
+            return
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 25
+        self.tip_window = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(True)
+        tw.wm_geometry(f"+{x}+{y}")
+        label = tk.Label(
+            tw,
+            text=self.text,
+            justify="left",
+            background="#ffffe0",
+            relief="solid",
+            borderwidth=1,
+            font=("tahoma", "8", "normal"),
+        )
+        label.pack(ipadx=1)
+
+    def hide_tooltip(self, event=None):
+        """Hide the tooltip."""
+        if self.tip_window:
+            self.tip_window.destroy()
+        self.tip_window = None
+
+
+class LinkwizGUI:
     def __init__(self, browsers: Dict[str, str], url: str):
         self.url = url
         self.hostname = urlparse(url).netloc
@@ -38,8 +75,26 @@ class LinkwizGUI:
 
     def _create_widgets(self):
         """Create widgets."""
+        self._create_url_label()
         self._create_buttons()
         self._create_remember_check()
+
+    def _create_url_label(self):
+        """Create a label to display the URL."""
+        display_url = self._get_display_url()
+        self.url_label = tk.Label(
+            self.root,
+            text=display_url,
+            bg="white",
+            fg="black",
+        )
+        self.url_label.pack(anchor=tk.W, padx=10)
+        ToolTip(self.url_label, self.url)
+
+    def _get_display_url(self):
+        """Get the URL with the path replaced by '~'."""
+        parsed_url = urlparse(self.url)
+        return f"{parsed_url.scheme}://{parsed_url.netloc}/~"
 
     def _create_buttons(self):
         """Create buttons for each browser."""
