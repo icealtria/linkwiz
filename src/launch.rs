7a4e8976bd91970db72e48a70cd885a446ef58d0
icealtria <icealtria@gmail.com>
2024-11-11 16:14:40 +0800

    fix?

--- src/launch.rs
+++ src/launch.rs
@@ -1,56 +1,34 @@
-use std::process::{Command, Stdio};
-
 use find_browsers::Browser;
+use std::process::{Command, Stdio};
 
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
 
 pub fn open_url_in_browser(url: &str, browser: &Browser) {
     let exec_path = browser.exec.to_str().unwrap();
-    let trimmed_exec = exec_path.trim();
-
-    let has_additional_args = trimmed_exec.contains(' ');
+    let encoded_url = url.replace("&", "%26");
 
     #[cfg(target_os = "windows")]
     {
-        if has_additional_args {
-            Command::new("cmd")
-                .creation_flags(0x08000000)
-                .arg("/C")
-                .arg(trimmed_exec)
-                .arg(url)
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .spawn()
-                .expect("Failed to open URL in browser");
-        } else {
-            Command::new(trimmed_exec)
-                .creation_flags(0x08000000)
-                .arg(url)
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .spawn()
-                .expect("Failed to open URL in browser");
-        }
+        Command::new("cmd")
+            .creation_flags(0x08000000)
+            .arg("/C")
+            .arg(exec_path)
+            .arg(encoded_url)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Failed to open URL in browser");
     }
 
     #[cfg(target_os = "linux")]
     {
-        if has_additional_args {
-            Command::new("sh")
-                .arg("-c")
-                .arg(format!("{} {}", trimmed_exec, url))
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .spawn()
-                .expect("Failed to open URL in browser");
-        } else {
-            Command::new(trimmed_exec)
-                .arg(url)
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .spawn()
-                .expect("Failed to open URL in browser");
-        }
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("{} {}", trimmed_exec, encoded_url))
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Failed to open URL in browser");
     }
 }
